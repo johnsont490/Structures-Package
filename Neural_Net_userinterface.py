cc8e6140cab9ff10d58427723c9fad8068f023d9
johnsont490 <86576362+johnsont490@users.noreply.github.com>
2022-12-13 22:45:56 -0500

    Add files via upload

---
+++ Neural_Net_userinterface.py
@@ -0,0 +1,7 @@
+#import relevant modules 
+from pathlib import Path
+from makehardnessprediction import makehardnessprediction
+#add input data as a relative path and define model type (cheenady or pugh)
+#call makehardnessprediction module to predict based on user input data, following the format below
+#makehardnessprediction(inputdata,modeltype)
+makehardnessprediction() 
